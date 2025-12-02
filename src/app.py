5ec0642f4771d3e367543eebcc9c87e2b62bce4b
1inoba <haruka.inoba@studium.uni-hamburg.de>
2025-12-02 17:08:33 +0100

    feat: changed user input

--- src/app.py
+++ src/app.py
@@ -18,26 +18,33 @@ st.sidebar.header('User Input Features')
 
 def user_input_features():
     pump_age = st.sidebar.slider('Age of pump (years)', 0, 55, 20)
+    gps_height = st.sidebar.slider('Height (m)', -90, 10, 2770)
     region = st.sidebar.selectbox('Region',('Iringa','Mara', 'Manyara', 'Mtwara', 'Kagera', 'Tanga',
                                             'Shinyanga', 'Tabora', 'Pwani', 'Ruvuma', 'Kilimanjaro', 'Rukwa',
                                             'Mwanza', 'Kigoma', 'Lindi', 'Dodoma', 'Arusha', 'Mbeya',
                                             'Singida', 'Morogoro', 'Dar es Salaam'))
-    water_quality = st.sidebar.selectbox('Water quality',('good', 'salty', 'milky', 'fluoride', 'colored', 'unknown'))
     water_quantity = st.sidebar.selectbox('Water quantity',('enough', 'insufficient', 'dry', 'seasonal', 'unknown'))
     payment_type = st.sidebar.selectbox('Payment type', ('Never pay', 'per bucket', 'monthly', 'annually', 'unknown', 
                                                    'on failure', 'other'))
     extraction_type = st.sidebar.selectbox('Extraction type', ('gravity', 'handpump', 'motorpump', 'other', 'rope pump',
                                                                 'submersible', 'wind-powered'))
-    management = st.sidebar.selectbox('Management organization', ('company', 'other', 'other - school', 'parastatal',
-                                                            'private operator', 'trust', 'unknown', 'vwc', 'water authority',
-                                                            'water board', 'wua', 'wug'))
-    data = {'region': region,
+    source = st.sidebar.selectbox('source', ('spring', 'rainwater harvesting', 'dam', 'machine dbh', 'other',
+                                            'shallow well', 'river', 'hand dtw', 'lake', 'unknown'))
+    basin = st.sidebar.selectbox('basin', ('Lake Nyasa', 'Lake Victoria', 'Pangani',
+                                            'Ruvuma / Southern Coast', 'Internal', 'Lake Tanganyika',
+                                            'Wami / Ruvu', 'Rufiji', 'Lake Rukwa'))
+    
+    data = {
+            'region': region,
             'pump_age': pump_age,
-            'quality_group': water_quality,
+            'gps_height': gps_height,
             'quantity_group': water_quantity,
             'payment_type': payment_type,
             'extraction_type_class': extraction_type,
-            'management': management}
+            'source': source,
+            'basin': basin
+            }
+    
     features = pd.DataFrame(data, index=[0])
     return features
 
